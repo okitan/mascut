05f99fbf2786f0fb194c36c452fd3e9369fbd2fe
okitan <okitakunio@gmail.com>
2010-04-08 23:13:25 +0900

    add Rack option to mascut

--- lib/mascut.rb
+++ lib/mascut.rb
@@ -1,4 +1,6 @@
 module Mascut
+  VERSION = File.read(File.join(File.dirname(__FILE__), %w[ .. VERSION ])).chomp
+  
   autoload :Mascut, 'mascut/mascut'
   autoload :Option, 'mascut/option'
 end
--- lib/mascut/option.rb
+++ lib/mascut/option.rb
@@ -1,4 +1,38 @@
+require 'optparse'
+
 module Mascut
   class Option
+    def self.parse(args = ARGV.dup)
+      {
+        :server => 'mongrel',
+        :Host   => '0.0.0.0',
+        :Port   => 9203
+      }.tap do |options|
+        OptionParser.new do |opts|
+          opts.separator 'Rack options:' # almost same as rack
+          opts.on('-s', '--server SERVER', 'serve using SERVER (default: mongrel)') { |s|
+            options[:server] = s
+          }
+          
+          opts.on('-o', '--host HOST', 'listen on HOST (default: 0.0.0.0)') { |host|
+            options[:Host] = host
+          }
+          
+          opts.on('-p', '--port PORT', 'use PORT (default: 9203)') { |port|
+            options[:Port] = port
+          }
+          
+          opts.on('-D', '--daemonize', 'run daemonized in the background') { |d|
+            options[:daemonize] = d ? true : false
+          }
+          
+          opts.separator ''
+          opts.separator 'Mascut options:(TODO)'
+          
+          opts.permute!(args)
+          options[:files] = args
+        end
+      end
+    end
   end
 end
