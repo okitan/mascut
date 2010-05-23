287f7ac6e83e30fa3374c0fea82ecad173794d1a
okitan <okitakunio@gmail.com>
2010-05-24 07:53:15 +0900

    correct spec

--- spec/mascut_spec.rb
+++ spec/mascut_spec.rb
@@ -1,8 +1,6 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe Mascut do
-  it 'should fail' do
-    subject::VERSION.should =~ /^\d+\.\d+\.\d+(\w+)?$/
-  end
+describe Mascut::VERSION do
+  it { should =~ /^\d+\.\d+\.\d+(\w+)?$/ }
 end
 
