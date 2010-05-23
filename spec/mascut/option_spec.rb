77b6dbe14d18508569cfee9efb4ea6ade39548a7
okitan <okitakunio@gmail.com>
2010-05-24 08:00:32 +0900

    add specs

--- spec/mascut/option_spec.rb
+++ spec/mascut/option_spec.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), %w[ .. spec_helper ])
 
 describe Mascut::Option, '#parse' do
+  subject { Mascut::Option.parse(args) }
   let(:value) { 'value' }
   
   context 'String option' do
@@ -8,7 +9,7 @@ describe Mascut::Option, '#parse' do
       %w[ -o --host   Host ],
     ].each do |short, long, key|
       context "with short option #{short}" do
-        subject { Mascut::Option.parse([ short, value ]) }
+        let(:args) { [ short, value ] }
         
         it "should assign #{key}" do
           subject[key.to_sym].should == value
@@ -61,12 +62,27 @@ describe Mascut::Option, '#parse' do
     end
   end
 
+  # out of options
+  context 'not options' do
+    let(:args) { %w[ file1 file2 ] }
+
+    it 'should be assigned to files' do
+      subject[:files].should == args
+    end
+  end
+
+  context 'invalid options' do
+    it 'should have specs'
+  end
+
+  # default values
   [ [ :server, 'mongrel' ],
     [ :Host,   '0.0.0.0' ],
-    [ :Port,    9203 ],
+    [ :Port,   9203 ],
+    [ :files,  nil ],
   ].each do |key, default_value|
     context "when #{key} is not specified" do
-      subject { Mascut::Option.parse([]) }
+      let(:args) { [] }
       
       it 'should be assigned default value' do
         subject[key].should == default_value
